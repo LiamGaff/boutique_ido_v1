b6ce4b2d645cd7885c0472e44f05dfd646d82ec1
LiamGaff <liamgaffneytya@gmail.com>
2021-08-25 10:43:34 +0000

    added checkout template

--- checkout/forms.py
+++ checkout/forms.py
@@ -2,14 +2,13 @@ from django import forms
 from .models import Order
 
 
-class OrderForm(froms.ModelForm):
+class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
-        fields = ( 'full_name', 'email',
-              'phone_number', 'country', 'postcode', 
-              'town_or_city', 'street_address1','street_address2', 
+        fields = ('full_name', 'email',
+              'phone_number', 'country', 'postcode',
+              'town_or_city', 'street_address1','street_address2',
               'county',)
-
 
     def __init__(self, *args, **kwargs):
             """
