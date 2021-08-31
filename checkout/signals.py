7fbf9c1e9e4cdca10a4e611d5def83e6132fbcd5
LiamGaff <liamgaffneytya@gmail.com>
2021-08-31 10:57:21 +0000

    Added checkout success logic

--- checkout/signals.py
+++ checkout/signals.py
@@ -13,7 +13,7 @@ def update_on_save(sender, instance, created, **kwardgs):
 
 
 @receiver(post_delete, sender=OrderLineItem)
-def update_on_save(sender, instance, **kwardgs):
+def update_on_delete(sender, instance, **kwardgs):
     """
     Update order total on inlineitem delete
     """
